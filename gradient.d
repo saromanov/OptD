38c2e5ad09e87e68308633889d44a6ea28daccb9
Sergey Romanov <xxsmotur@gmail.com>
2014-06-27 23:56:06 +0600

    Append subgradient

--- gradient.d
+++ gradient.d
@@ -1,7 +1,9 @@
 module gradient;
 
 alias float function(double) Func;
+alias float function(double, double) Subgradient;
 alias double[] Arr;
+
 
 template Grad(T){
 	T[] gradient(T x0, float alpha, uint iters, Func dfunc,float stepsize=1e-3){
@@ -12,11 +14,33 @@ template Grad(T){
 		}
 		return result;
 	}
+
+	T[] subgradient(T x0, Func alpha, uint iters, real function(real) subgrad){
+		double[] result = new double[](iters+1);
+		T bestvalue;
+		result[0] = x0;
+		for(int i = 0;i < iters;++i){
+			T current = result[i] - alpha(result[i]) * subgrad(result[i]);
+			result[i+1] = current;
+			if(current < bestvalue)
+				bestvalue = current; 
+		}
+		return result;
+	}
 }
 
 
 double[] GradientDescent(double x0, float alpha, uint iters, 
-	Func dfunc, float stepsize=1e-3){
+	Func dfunc){
 	return Grad!double.gradient(x0, alpha, iters, dfunc);
+}
+
+
+//http://en.wikipedia.org/wiki/Subgradient_method
+//http://see.stanford.edu/materials/lsocoee364b/01-subgradients_notes.pdf
+double[] SubGradientDescent(double x0, Func alpha, uint iters, 
+	real function(real) subgrad, 
+	float stepsize=1e-3){
+	return Grad!double.subgradient(x0, alpha, iters, subgrad);
 }
 
--- optd.d
+++ optd.d
@@ -4,6 +4,9 @@ import std.stdio;
 import std.math;
 import gradient;
 
+//TODO
+//Merge gaussnewton and this project
+//add Levenberg–Marquardt_algorithm
 
 float dfunc(double x){
 		return pow(x,3);
@@ -13,7 +16,13 @@ void test_gradient(){
 	writeln(GradientDescent(0.74, 0.5,100, &dfunc));
 }
 
+void test_subgradient(){
+	auto stepsize = function float (double x){ return 1e-4;};
+	auto result = SubGradientDescent(0.5, stepsize, 100, &tanh);
+	writeln(result);
+}
+
 void main()
 {
-
+	test_subgradient();
 }
