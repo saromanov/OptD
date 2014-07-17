54b77c3334b4afca9c52cb7445103275d0b74ba9
Sergey Romanov <xxsmotur@gmail.com>
2014-07-17 23:57:34 +0600

    Set main func in class

--- quasinewton.d
+++ quasinewton.d
@@ -1,28 +1,59 @@
 module quasinewton;
 import matrix;
+import std.math;
+import std.stdio;
+import std.range;
 
-T[][] computeMatrix(T[][] matr, T[] diff, T[] outfunc){
+
+////http://en.wikipedia.org/wiki/BFGS_method
+//http://terminus.sdsu.edu/SDSU/Math693a_f2013/Lectures/18/lecture.pdf
+class BFGS(T){
+
+float[string] variables;
+
+T[][] computeMatrix(T)(T[][] matr, T[] diff, T[] outfunc){
 	T[] ch = matr.product(diff).product(diff.T()).product(diff);
 	T[] zn = diff.T().product(matr).product(diff);
 	T[] one = outfunc.product(outfunc.T());
 	T[] two = outfunc.T().product(diff);
 }
 
-//http://en.wikipedia.org/wiki/BFGS_method
-//http://terminus.sdsu.edu/SDSU/Math693a_f2013/Lectures/18/lecture.pdf
-T[] BFGS(T)(T x0, float elsilon, T function(T) func,
-	T function(T) dfunc, float[string] vars,
-	int iters){
-	T[][] aprhessian = new T[][](iters, iters);
-	T[]direction = new T[](iters);
-	T[][] hessian = new T[][](iters, vars.keys.length);
-	T[] values = new T[](iters);
-	T[0] = x0;
-	for(int i = 0;i < iters;++i){
-		direction[i] = aprhessian[i] * dfunc(T[i]);
-		T[i + 1] = T[i] + alpha * direction[i];
-		T diff = T[i + 1] - T[i];
-		T outfunc = dfunc(T[i + 1]) - dfunc(T[i]);
+T[][] initMatrix(T)(T[] diff, T[] output) nothrow in {
+	assert(diff.length > 0);
+	assert(output.length > 0);
+} body {
+	int len = output.length;
+	auto result = new T[][](len, len);
+	return result;
+}
+public:
+	this(){
 
 	}
+	void addVariable(string varname, float value){
+		variables[varname] = value;
+	}
+
+	void addFunc(T delegate(T value) func){
+
+	}
+
+	T[][] run(T[] x0, float epsilon, T function(T) func, T[] function(T[]) dfunc, float[string] vars,
+	int iters){
+	T[][] aprhessian = new T[][](iters, iters);
+	T[][]direction = new T[][](iters, iters);
+	T[][] hessian = new T[][](iters, vars.keys.length);
+	T[][] values = new T[][](iters, iters);
+	values[0] = x0;
+	//hessian = initMatrix(values, values);
+	for(int i = 0;i < iters;++i){
+		direction[i] = hessian.product(dfunc(values[i]));
+		/*values[i + 1] = values[i] + direction[i].product(epsilon);
+		T[] diff = values[i + 1] - values[i];
+		T outfunc = dfunc(values[i + 1]) - dfunc(values[i]);*/
+
+		}
+	return values;
+	}
+
 }
