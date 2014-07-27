732b734e93a83c24eaa81069e3a714a7c8ac29b2
Sergey Romanov <xxsmotur@gmail.com>
2014-07-27 23:26:09 +0600

    Next to implement BHHH algorithm

--- BHHH.d
+++ BHHH.d
@@ -1,29 +1,47 @@
 import std.stdio;
 import std.typecons;
-import la;
+import std.range;
+import la.matrix;
 
 //Implementation of Berndt–Hall–Hall–Hausman algorithm
 //http://en.wikipedia.org/wiki/BHHH_algorithm
 
-alias T function(T,T) OFUNC;
 
-T [][] BHHH(T)(T x0, OFUNC dfunc, int iters_limit){
+
+T [][] BHHH(T)(T function(T,T) dfunc, T[string] variables, int iters_limit){
 	int iter = 1;
 	T[] lambdas = StepSize(iters_limit, 0.75);
-	T[] result = new T[](iters_limit.length);
+	int varlength = variables.keys.length;
+	writeln(varlength);
+	T[][] result = new T[][](iters_limit+1, varlength);
+	result[0] = variables.values.array;
 	while(iter < iters_limit){
-		auto grad = dfunc(x0,x0);
+		auto newvalues = new T[](varlength);
+		int c = 0;
+		foreach(ref i; variables.keys){
+			newvalues[c] = dfunc(variables[i], variables[i]);
+			c += 1;
+		}
 		auto result1 = grad.prod(grad).inv();
 		result1[iter + 1] = result1[iter].prod(lambdas[iter]);
 		iter += 1;
 
 	}
+	return result;
+}
+
+unittest {
+	double[string] variables;
+	variables["A"] = 0.025;
+	variables["B"] = 0.999;
+	auto func = function(double x, double y){ return 2 * x;};
+	auto bhhh = BHHH!double(func, variables, 100);
 }
 
 //Append line search
-T[] StepSize(T)(int count, T value=null){
-	if(value is null)
-		return count.iota.array;
-	else 
-		count.iota.map!(x => value).array;
+T[] StepSize(T)(int count, T value=0){
+	if(value is 0)
+		return repeat(1e-5,10).array;
+	
+	return count.iota.map!(x => value).array;
 }
